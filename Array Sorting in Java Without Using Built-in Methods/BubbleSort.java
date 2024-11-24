ffd339c1e1bd2d4ee8ef70b32f4d9490fca62f30
vishal-rakhonde <vrakhondepatil@gmail.com>
2024-11-24 09:57:29 +0530

    Easy

--- Array Sorting in Java Without Using Built-in Methods/BubbleSort.java
+++ Array Sorting in Java Without Using Built-in Methods/BubbleSort.java
@@ -1,10 +1,19 @@
+import java.util.Scanner;
 public class BubbleSort {
     public static void main(String[] args) {
-        int[] arr = {5, 3, 8, 6, 2, 7};
-        
-        for (int i = 0; i  arr.length - 1; i++) {
-            for (int j = 0; j  arr.length - i - 1; j++) {
-                if (arr[j]  arr[j + 1]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
@@ -12,9 +21,9 @@ public class BubbleSort {
             }
         }
 
-        System.out.print(Sorted Array );
-        for (int num  arr) {
-            System.out.print(num +  );
+        System.out.print("Sorted Array: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
         }
     }
 }
